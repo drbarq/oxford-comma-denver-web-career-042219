97927a2cbab74ac11e8d1219717d76c61f6d0b18
Joe Tustin <j.tustin@gmail.com>
2019-03-13 21:25:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
     format_array << array.join
     return format_array
 end 
-  
+
   format_array << array.join(", ")
   format_array[format_array.rindex(" ")] = " and "
   return format_array
