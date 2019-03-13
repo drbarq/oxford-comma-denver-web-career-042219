f7d9238a6b46c15b65389e71c1bf0ea7d0c30b78
Joe Tustin <j.tustin@gmail.com>
2019-03-13 20:43:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,13 @@
 def oxford_comma(array)
+  case array 
+  when array.length === 1 
+    return array.join 
+  when array.length > 1
+    return array.join(" and ")
+
+
+
+    
   return array.join 
 
 end
