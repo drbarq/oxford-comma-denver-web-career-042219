a8b4d5e4970f500d65892e135d4419ac5b943108
Joe Tustin <j.tustin@gmail.com>
2019-03-13 20:48:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,8 @@
 def oxford_comma(array)
   format_array = Array.new
-  array.each {|word| format_array << array.split(",")}
+
+  format_array << array.join
+  format_array.each {|word| format_array << array.split(",")}
   return format_array
 
 
