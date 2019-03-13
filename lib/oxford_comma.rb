61af3b6064c3ff73fc1bbd064266521e41b49f02
Joe Tustin <j.tustin@gmail.com>
2019-03-13 21:35:36 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -27,7 +27,7 @@ def oxford_comma(array)
     format_array << array.join(", ")
     if format_array.count(",") <= 1
       format_array[format_array.rindex(", ")] = " and"
-
+      return format_array
 
     format_array[format_array.rindex(", ")] = ", and"
     return format_array
