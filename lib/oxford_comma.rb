a5f102c2207e4e78045f6026897232ee02e6346c
Joe Tustin <j.tustin@gmail.com>
2019-03-13 20:54:17 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,8 +3,11 @@ def oxford_comma(array)
 
   format_array << array.join(",")
   ##return format_array
-  format_array.each {|word| format_array << array.split(",")}
+  ##format_array.each {|word| format_array << array.split(",")}
  return format_array
 
 
 end
+
+
+oxford_comma(["kiwi", "durian", "starfruit"])
