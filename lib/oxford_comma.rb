62080525200cfb187efd82d376705d448068a6a6
Joe Tustin <j.tustin@gmail.com>
2019-03-13 21:05:04 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,6 +6,12 @@ def oxford_comma(array)
 ##  format_array.each {|word| format_array << array.split(",")}
 ##  if array.length > 1
   puts format_array
+
+  ## count the number of spaces and inject the and
+
+  puts format_array.count(" ")
+  puts format_array.index(" ")
+
   return format_array.length
 
 
