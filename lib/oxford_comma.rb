8a733cb87b014156a65dc4fc5fbe4eac3f6671fc
Joe Tustin <j.tustin@gmail.com>
2019-03-13 21:47:53 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -19,6 +19,12 @@ end
 ##oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits"])
 ##oxford_comma(["kiwi"])
 
+
+## tried a few different ways to get it smaller and more concise 
+## landed on counting the " , " 
+## first comnvert the array to a string joining the elements with a comma
+## 
+
 def oxford_comma(array)
   format_array = String.new
   format_array << array.join(", ")
