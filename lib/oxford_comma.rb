ada93e10bec784922b0164a1d71965656cffb903
Joe Tustin <j.tustin@gmail.com>
2019-03-13 21:42:01 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -22,12 +22,12 @@ end
 def oxford_comma(array)
   format_array = String.new
   format_array << array.join(", ")
-    if format_array.count(",") >= 1
+    if format_array.count(",") <= 1
       format_array[format_array.rindex(", ")] = " and"
       return format_array
+    else
+      format_array[format_array.rindex(", ")] = ", and"
+      return format_array
     end
-
-    format_array[format_array.rindex(", ")] = ", and"
-    return format_array
-
+  return format_array
 end
