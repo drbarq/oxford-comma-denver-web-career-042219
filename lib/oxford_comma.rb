f838d2c40c62b87e2c005d47576848cefbd7a037
Joe Tustin <j.tustin@gmail.com>
2019-03-13 21:23:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,24 +1,9 @@
 def oxford_comma(array)
   format_array = String.new
-
   format_array << array.join(", ")
-  ##return format_array
-##  format_array.each {|word| format_array << array.split(",")}
-##  if array.length > 1
+##  puts format_array
+  format_array[format_array.rindex(" ")] = " and "
   puts format_array
-
-  ## count the number of spaces and inject the and
-
-##  puts format_array.count(" ")
-##  puts format_array.index(" ")
-##  puts format_array.rindex(" ")
-##  puts format_array.rindex(" ")
-  format_array[format_array.rindex(" ")] = " and"
-  puts format_array
-##  puts format_s
-##  return format_array.length
-
-
 end
 
 
